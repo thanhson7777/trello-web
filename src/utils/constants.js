c2b7443f1630f2c25f0955f1fdf3e703869cde37
thanhson7777 <thanhson11052003@gmail.com>
2026-01-13 16:07:59 +0700

    fix BUILD_MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,15 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+
+console.log('import.meta.env: ', import.meta.env)
+console.log('process.env: ', process.env)
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-zi88.onrender.com'
+}
+console.log('🚀 ~ apiRoot:', apiRoot)
+export const API_ROOT = apiRoot
 // export const API_ROOT = 'https://trello-api-zi88.onrender.com'
